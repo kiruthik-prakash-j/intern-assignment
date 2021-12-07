6b37aead9497f6ea18e43b4f1c22529cb5e7947d
Kiruthik Prakash J <kiruthikprakashj@gmail.com>
2021-12-07 14:40:03 +0530

    buildspec Updated

--- php/code/config.php
+++ php/code/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'toor1234');
-define('DB_NAME', 'demo');
+define('DB_SERVER', getenv("DB_HOST"));
+define('DB_USERNAME', getenv("MYSQL_USER"));
+define('DB_PASSWORD', getenv("MYSQL_PASSWORD"));
+define('DB_NAME', getenv("MYSQL_DATABASE"));
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
